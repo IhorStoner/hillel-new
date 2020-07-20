3c57ce5c555f07d71e0b66d9a060075262c3bffa
Vadim Oleynik <vadimoleynik039@gmail.com>
2020-07-20 21:31:19 +0700

    send product name to next page

--- source/js/function.js
+++ source/js/function.js
@@ -56,34 +56,63 @@ const createCards = (arr, parent) => {
     parent.appendChild(shopCards);
 
     arr.forEach(item => {
-
-        const productCard = document.createElement(`div`);
-
-        productCard.classList.add(`product`);
-
-        productCard.innerHTML = `
-
-            <div class="product__img-container">
-                    <img src="${item.img}" alt="" class="product__img">
-            </div>
-            <div class="product__content">
-                    <h3 class="product__title">
-                        ${item.name}
-                    </h3>
-                    <p class="product__text">Premium beard balm</p>
-                    <p class="product__text">50 ml</p>
-                    <p class="product__text">new york original</p>
-                    <p class="product__price">
-                        ${item.price}$
-                    </p>
-                    <div class="product__buttons">
-                        <button class="btn btn--black">Shop now</button>
-                        <button class="btn btn--plus"></button>
-                    </div>
-
-    `;
-        shopCards.appendChild(productCard);
-
-        document.querySelectorAll(`.product__img`).forEach(item => { item.style.width = `100%` });
+        createCard(item, shopCards, arr);
     });
+}
+
+const createCard = (item, parent, arr) => {
+
+    const productCard = document.createElement(`div`);
+
+    productCard.classList.add(`product`);
+
+    productCard.innerHTML = `
+
+        <div class="product__img-container" data-id=${item.id}>
+                <img src="${item.img}" alt="" class="product__img">
+        </div>
+        <div class="product__content" data-id=${item.id}>
+                <h3 class="product__title">
+                    ${item.name}
+                </h3>
+                <p class="product__price">
+                    ${item.price}$
+                </p>
+                <div class="product__buttons">
+                    <button class="btn btn--black">Shop now</button>
+                    <button class="btn btn--plus"></button>
+                </div>
+
+`;
+    parent.appendChild(productCard);
+
+    //исправить !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    document.querySelectorAll(`.product__img`).forEach(item => { item.style.width = `100%` });
+
+    productCard.addEventListener(`click`, handlerProductItem.bind(null, arr))
+
+}
+
+//нажатие на товар, переход на детальную информацию о товаре
+const handlerProductItem = (arr, event) => {
+    const target = event.target;
+
+    if (target.tagName === `BUTTON`) {
+        return;
+    }
+
+    const selectedProductId = target.parentNode.getAttribute(`data-id`);
+
+    const selectedProduct = arr.find((element) => {
+        return element.id === Number(selectedProductId);
+    });
+
+
+    showDetailInfo(selectedProduct);
+}
+
+
+//отображение страници с товаром
+const showDetailInfo = (item) => {
+    document.querySelector(`.shop .wrapper .shop-content .shop__flex-container`).innerHTML = ``;
 }
